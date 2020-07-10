2166747142821ec0f1510a8c52ce8026cc0179a3
Сергей Шурин <s.shurin94@gmail.com>
2020-07-11 02:44:49 +0300

    initial templates

---
+++ frontend/layouts/error.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  div.container
+    h1(v-if="error.statusCode === 404") Страница не найдена
+    h1(v-else) Произошла ошибка
+    nuxt-link.button--green(to="/") Вернуться на главную
+</template>
+
+<script>
+export default {
+  props: ['error']
+}
+</script>
+
+<style lang="scss">
+.container {
+  flex-direction: column;
+  h1 {
+    margin-bottom: 20px;
+  }
+}
+</style>
