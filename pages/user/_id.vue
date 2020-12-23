d205a25adc5137b6b5e6415ff35a3b0c3df20def
Сергей Шурин <s.shurin94@gmail.com>
2020-12-24 02:33:31 +0300

    user page css module

--- pages/user/_id.vue
+++ pages/user/_id.vue
@@ -1,14 +1,18 @@
 <template lang="pug">
-  div#user-page(
+  div(
+    :class="$style.user_page"
   )
-    div.main-images
+    div(
+      :class="$style.main_images"
+    )
       CustomImage(
         :width="mobile ? '100%' : '200px'"
         :height="mobile ? '100%' : '300px'"
         :src="user ? user.images.filter(img => img.avatar)[0].src : ''"
       )
-      div.icons(
+      div(
         v-if="!myPage"
+        :class="$style.icons"
       )
         Like(
           :size="2"
@@ -22,42 +26,59 @@
           :size="2"
           :user="user"
         )
-      Button.chat-link(
+      Button(
         v-if="chatAvailable"
+        :class="$style.chat_link"
         @click="() => $router.push(`/chat/${user.login}`)"
         :user="user"
       ) Join to chat
       
-    div.info
-      div.field
-        span.fio
+    div(
+      :class="$style.info"
+    )
+      div(
+        :class="$style.field"
+      )
+        span(
+          :class="$style.fio"
+        )
           NameLink(
             :user="user"
           )
-          Raiting.rate(
+          Raiting(
+            :class="$style.rate"
             :value="user ? user.rating : undefined"
             :size="1"
           )
-          Online.online.only_laptop(
+          Online(
             v-if="user"
+            :class="$style.online"
             :time="new Date(this.user.time)"
           )
 
       div.title.left Gender
-      div.field.gender {{ ['male', 'female', 'bisexual'][user.gender - 1] }}
+      div(
+        :class="[$style.field, $style.gender]"
+      ) {{ ['male', 'female', 'bisexual'][user.gender - 1] }}
 
       div.title.left Preferences
-      div.field.preferences
+      div(
+        :class="[$style.field, $style.preferences]"
+      )
         template(v-if="user")
           span {{ ['male', 'female', 'bisexual'][user.preference - 1] }}
         template(v-else)
           span ...
 
       div.title.left Biography
-      div.field.biography {{ user ? user.biography : '...' }}
+      div(
+        :class="[$style.field, $style.biography]"
+      ) {{ user ? user.biography : '...' }}
 
       div.title.left Tags
-      div.field.tags
+      div(
+        :class="[$style.field, $style.tags]"
+      )
         template(v-if="user")
           Tag(
             v-for="tag in user.tags"
@@ -68,9 +89,12 @@
           span ...
 
       div.title.left Images
-      div.field.images
+      div(
+        :class="[$style.field, $style.images]"
+      )
         template(v-if="user")
-          CustomImage.image(
+          CustomImage(
+            :class="$style.image"
             height="100px"
             width="100px"
             v-for="img in user.images"
@@ -83,17 +107,17 @@
 </template>
 
 <script>
-import { mapActions, mapGetters, mapMutations } from 'vuex';
-import CustomImage from '@/components/CustomImage.vue';
-import NameLink from '@/components/NameLink.vue';
-import Button from '@/components/Button.vue';
-import Tag from '@/components/Tag.vue';
-import Ban from '@/components/Ban.vue';
-import Block from '@/components/Block.vue';
-import Like from '@/components/Like.vue';
-import Distance from '@/components/Distance.vue';
-import Raiting from '@/components/Raiting.vue';
-import Online from '@/components/Online.vue';
+import { mapActions, mapGetters, mapMutations } from 'vuex'
+import CustomImage from '@/components/CustomImage.vue'
+import NameLink from '@/components/NameLink.vue'
+import Button from '@/components/Button.vue'
+import Tag from '@/components/Tag.vue'
+import Ban from '@/components/Ban.vue'
+import Block from '@/components/Block.vue'
+import Like from '@/components/Like.vue'
+import Distance from '@/components/Distance.vue'
+import Raiting from '@/components/Raiting.vue'
+import Online from '@/components/Online.vue'
 
 export default {
   name: 'UserPage',
@@ -123,7 +147,7 @@ export default {
       mobile: 'IS_MOBILE',
     }),
     myPage: function() {
-      return this.user ? this.$auth.user.login === this.user.login : true;
+      return this.user ? this.$auth.user.login === this.user.login : true
     },
     chatAvailable: function() {
       return this.user ? this.user.likedFrom && this.user.likedTo : false
@@ -131,10 +155,8 @@ export default {
   },
   methods: {
     ...mapMutations({
-      chatWIthUser: 'users/SET_CUR_CHAT_USER_ID',
     }),
     ...mapActions({
-      visitUser: 'user/VISIT_USER',
     }),
   },
   mounted() {
@@ -142,76 +164,87 @@ export default {
 };
 </script>
 
-<style scoped lang="scss">
+<style module lang="scss">
 
-#user-page {
-  padding: 50px 10px;
-  display: flex;
-  @media (max-width: map-get($grid-breakpoints, sm)) {
-    flex-direction: column;
-    padding: 0 0 50px 0;
-  }
-  .main-images {
-    margin-bottom: 20px;
-    .icons {
-      color: lightgray;
-      display: flex;
-      position: relative;
-      justify-content: space-around;
-    }
-    .chat-link {
-      position: relative;
-      top: 10px;
-    }
-    @media (max-width: map-get($grid-breakpoints, sm)) {
-      .chat-link {
+@mixin userMixin(
+  $padding,
+  $flex-direction: row,
+  $width-info,
+  $margin-left-info: 0,
+  $online-display: block,
+) {
+
+  .user_page {
+    padding: $padding;
+    display: flex;
+    flex-direction: $flex-direction;
+    .main_images {
+      margin-bottom: 20px;
+      .icons {
+        color: lightgray;
+        display: flex;
+        position: relative;
+        justify-content: space-around;
+      }
+      .chat_link {
+        position: relative;
+        top: 10px;
+        width: 100%;
         display: block;
         width: 90%;
         margin: 0 auto;
       }
     }
-    @media (min-width: map-get($grid-breakpoints, sm)) {
-      .chat-link { width: 100%; }
-    }
-  }
-  .info {
-    @media (min-width: map-get($grid-breakpoints, sm)) { width: 600px; };
-    @media (min-width: map-get($grid-breakpoints, sm)) { margin-left: 30px; }
-    .field {
-      position: relative;
-      width: 100%;
-      padding: 10px 5%;
-      .fio {
-        display: flex;
-        align-items: center;
-        flex-wrap: wrap;
-        @media (max-width: map-get($grid-breakpoints, sm)) { flex-direction: column; };
+    .info {
+      width: $width-info;
+      margin-left: $margin-left-info;
+      .field {
         position: relative;
-        font-family: 'Lobster', cursive;
-        font-size: 20px;
-        .name,
-        .rate,
-        .dist {
-          margin: 5px 20px 5px 0;
-        }
-        .online {
-          @media (min-width: map-get($grid-breakpoints, sm)) { margin-left: auto; };
+        width: 100%;
+        padding: 10px 5%;
+        .fio {
+          display: flex;
+          align-items: center;
+          flex-wrap: wrap;
+          flex-direction: $flex-direction;
+          position: relative;
+          font-family: 'Lobster', cursive;
+          font-size: 20px;
+          .rate {
+            margin-left: 10px;
+          }
+          .online {
+            display: $online-display;
+            margin-left: auto;
+          }
         }
       }
-    }
-    .tags,
-    .preferences {
-      span {
-        margin-right: 10px;
+      .tags,
+      .preferences {
+        span {
+          margin-right: 10px;
+        }
       }
-    }
-    .tags span {
-      cursor: default;
-    }
-    .image {
-      margin: 5px;
+      .image {
+        margin: 5px;
+      }
     }
   }
+}
+
+@include userMixin(
+  $padding: 50px 10px,
+  $width-info: 600px,
+  $margin-left-info: 30px,
+);
+
+@media (max-width: 600px) {
+  @include userMixin(
+    $padding: 0 0 50px 0,
+    $flex-direction: column,
+    $width-info: 100%,
+    $online-display: none,
+  );
 }
 
 </style>
